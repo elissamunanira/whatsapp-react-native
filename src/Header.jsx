d1a803225328b4603f0b98054d2970f0ccb069c9
elissamunanira <munanira64@gmail.com>
2024-05-12 22:03:38 +0200

    search

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState} from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { Feather, Fontisto, MaterialCommunityIcons } from 'react-native-vector-icons';
 
 
 const Header = () => {
+  const [searchMode,setSearchMode] = useState('false');
+  const [searchQuery,setSearchQuery] = useState('');
+  const handleSearchIconClick = () => {
+    setSearchMode(!searchMode);
+  }
   return (
     <View style={styles.Container}>
         <View style={styles.headerContainer}>
             <Text style={styles.headerText}>WhatsApp</Text>
             <View style={styles.iconContainer}>
                 <Feather  name='camera' size={20} color ='#fff'style={styles.icon}/>
+                <TouchableOpacity  onPress={handleSearchIconClick}>
                 <Fontisto name = 'search'size={20} color ='#fff'style={styles.icon}/>
+                </TouchableOpacity>
                 <MaterialCommunityIcons name ='dots-vertical'size ={21} color='#fff'style={styles.icon} />
             </View>
         </View>
+        {searchMode && (
+          <TextInput
+          style = {styles.searchInput} placeholder = "search..." value = {searchQuery} onchangeText = {text =>setSearchQuery(text)} autoFocus = {true} />
+        )}
     </View>
   )
 }
@@ -45,5 +56,13 @@ const styles = StyleSheet.create({
       },
       icon: {
         marginLeft: 20, 
+      },
+      searchInput: {
+        backgroundColor: '#fff',
+        marginHorizontal: 16,
+        marginTop: 10,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        borderRadius: 8,
       }
     });
